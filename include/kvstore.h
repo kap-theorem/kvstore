ed3075203de38854f1fc0e9c122b0d8e9ec72cd2
Pulasthya <pulasthyareddy@gmail.com>
2026-02-16 17:10:07 -0600

    intial setup

---
+++ include/kvstore.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <optional>
+
+class KVStore {
+public:
+  KVStore();
+
+  ~KVStore();
+
+    void Put(const std::string& key, const std::string& value);
+
+    std::optional<std::string> Get(const std::string key);
+};
